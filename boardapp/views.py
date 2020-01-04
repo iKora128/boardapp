daaa7bd68df005934e2199abe033c03e4b9d42ec
長嶋大地 <nagashimadaichi@nagashimadaichinoMacBook-Pro.local>
2020-01-04 14:34:51 +0900

    make detail.html

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import redirect
+from .models import BoardModel
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def signupfunc(request):
+    if request.method == "POST":
+        username2 = request.POST["username"]
+        password2 = request.POST["password"]
+        try:
+            User.objects.get(username=username2)
+            return render(request, "signup.html", {'error':"このユーザーは登録されています。"})
+        except:
+            user = User.objects.create_user(username, '', password)
+            return render(request, "signup.html", {"some": 100})
+    return render(request, "signup.html", {"some": 100})
+
+
+def loginfunc(request):
+    if request.method == "POST":
+        username2 = request.POST["username"]
+        password2 = request.POST["password"]
+        user = authenticate(request, username=username2, password=password2)
+        if user is not None:
+            login(request, user)
+            return redirect("list")
+        else:
+            return redirect("login")
+    return render(request, "login.html", {"some": 100})
+
+@login_required
+def listfunc(request):
+    object_list = BoardModel.objects.all()
+
+    return render(request, "list.html", {"object_list": object_list})
+
+
+def logoutfunc(request):
+    logout(request)
+    return redirect("login")
+
+def detailfunc(request, pk):
+    object = BoardModel.objects.get(pk=pk)
+    return render(request, "detail.html", {"object": object})
+
+
+
+
